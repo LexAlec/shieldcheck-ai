3743f5aacdc1518bf0a5c58add9911c03723cebf
Alex Da Costa Silva Tomé <alexdacostacosta@gmail.com>
2026-03-10 12:42:22 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4855774866-e5bed",
+  "appId": "1:388066742234:web:09bc451937b96810fdbae3",
+  "apiKey": "AIzaSyBbLxFyKcE9BEPykyMK7j5s8GuL5Uylcr4",
+  "authDomain": "studio-4855774866-e5bed.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "388066742234"
+};
